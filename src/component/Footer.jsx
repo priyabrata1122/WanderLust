0aac661d2ae84f7f5276f43afd5f26fea4befb2f
priyabrata1122 <priyabratass64@gmail.com>
2025-06-29 01:20:38 +0530

    Updates in CreateTrip Route

---
+++ src/component/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css'
+
+function Footer() {
+  return (
+    <div className="footer">
+        <h1>Copyright(c) 2025 WanderLust</h1>
+        <h1>Made with &#x2764; by Priyabrata</h1>
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/CreateTrip.jsx
+++ src/pages/CreateTrip.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 import { Input } from '../components/ui/input'
 import { SelestBudgetOptions , SelectTravelesList} from '../constants/options';
+import '../styles/Style_createTrip.css';
 
 function CreateTrip() {
 
@@ -32,103 +33,96 @@ function CreateTrip() {
         <div
             style={{
                 position: "relative",
-                top: "3rem",
-                width: "70rem",
-                left: "12rem",
-            }}
-            className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-25'>
-            <h2 className='font-bold text-3xl'>Tell us your travel preferences ⛺🌴</h2>
-            <p className='mt-3 text-gray-500 text-xl'> Just provide some basic information , and our trip planner will generate a <br /> customized itinerary based on your preferences</p>
-
-
-            <div className='flex flex-col gap-8'>
-                <div className='mt-20 '>
-                    <h2 className='text-xl my-3 font-medium'>What is destination of choice ? </h2>
-                    <GooglePlacesAutocomplete
-                        apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
-                        selectProps={{
-                            place,
-                            onChange: (v) => {
-                                setPlace(v);
-                                handleOnChange('location',v);
+                width: "96%",
+                left: "1.5rem",
+                top:"5rem",
+                height:'57rem'
+            }} className='maindiv flex-1'>
+                <h1
+                style={{
+                    position:'fixed',
+                    fontSize:'2.7rem',
+                    top:'20rem',
+                    left:'3rem',
+                    color:'#5e5e5e'
+                }}>Got a Vacation up? <br /> Start here by filling details</h1>
+                <div className='insidediv'>
+                    <h1 className='text-4xl ml-15 mb-5 font-serif'>Tell us your travel preferences ⛺🌴</h1>
+                    <p className='text-xl text-gray-600 mb-5'> Just provide some basic information , and our trip planner will generate a <br /> customized itinerary based on your preferences</p>
+                    <div>
+                        <div>
+                            <h2 className='text-xl font-serif mb-2'>What is destination of choice ? </h2>
+                            <GooglePlacesAutocomplete
+                                apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
+                                selectProps={{
+                                    place,
+                                    onChange: (v) => {
+                                        setPlace(v);
+                                        handleOnChange('location',v);
+                                    }
+                                }}
+                            />
+                        </div>
+                        <div>
+                            <h2 className='mt-7 text-xl font-serif mb-2'>How many days are you staying ? </h2>
+                            <input className='border-gray border-2 rounded-sm w-full h-10 p-2 mb-7'
+                            onChange={
+                                (e)=>{
+                                    handleOnChange('noOfDays',e.target.value)
+                                }
                             }
-                        }}
-                    />
+                            placeholder={'Example - 3'} type="number"></input>
+                        </div>
 
-                </div>
-                <div>
-                    <h2 className='text-xl my-3 font-medium'>How many days are you staying ? </h2>
-                    <Input 
-                    onChange={
-                        (e)=>{
-                            handleOnChange('noOfDays',e.target.value)
-                        }
-                    }
-                    placeholder={'Example - 3'} type="number"></Input>
-                </div>
+                        <div>
+                            <h2 className='text-xl font-serif mb-4'>What is your budget ?</h2>
+                            <div className='flex columns-3 justify-between'>
+                                {
+                                    SelestBudgetOptions.map((items, index) => (
+                                        <div 
+                                        className={`border-solid border-1 p-2 rounded-md cursor-pointer w-56 hover:shadow-lg
+                                            ${formdata?.budget==items.title && 'border-black shadow-lg'}`}
+                                        onClick={()=>{
+                                            handleOnChange('budget',items.title);
+                                        }}
+                                        key={index}>
+                                            <h1 className='text-2xl'>{items.icon}</h1>
+                                            <h1 className='font-bold text-xl'>{items.title}</h1>
+                                            <h4 className='text-gray-500'>{items.desc}</h4>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div>
 
-                <div className='mt-10'>
-                    <h2 className='text-xl my-3 font-medium'>What is your budget ?</h2>
-                    <div className='grid grid-cols-3 gap-5 mt-3'>
-                        {
-                            SelestBudgetOptions.map((items, index) => (
-                                <div 
-                                onClick={()=>{
-                                    handleOnChange('budget',items.title);
-                                }}
-                                key={index} 
-                                className={`p-4 border rounded-lg hover:shadow-lg cursor-pointer
-                                ${formdata?.budget==items.title && 'shadow-lg border-black'}`}>
-                                    <h1>{items.icon}</h1>
-                                    <h1 className='text-2xl font-bold'>{items.title}</h1>
-                                    <h4 className='text-l text-gray-500'>{items.desc}</h4>
-                                </div>
-                            ))
-                        }
+                        <div>
+                            <h2 className='mt-7 text-xl font-serif mb-2'>Who do you plan to travel with ?</h2>
+                            <div className='flex w-full flex-wrap justify-between'>
+                                {
+                                    SelectTravelesList.map((items, index) => (
+                                        <div 
+                                        className={`border-solid border-1 p-2 rounded-md cursor-pointer mb-3 w-56 hover:shadow-lg
+                                            ${formdata?.people==items.peple && 'border-black shadow-lg'}`}
+                                        onClick={()=>{
+                                            handleOnChange('people',items.peple)
+                                        }}
+                                        key={index}>
+                                            <h1 className='text-2xl'>{items.icon}</h1>
+                                            <h1 className='text-xl font-bold'>{items.title}</h1>
+                                            <h4 className='text-gray-500'>{items.desc}</h4>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className=''>
+                        <button className='left-150 relative mb-3 border-black border-1 pt-1 pb-1 pl-2 pr-2 rounded-md text-lg bg-black text-white cursor-pointer' onClick={()=>{generateTrip()}}>Gererate Trip</button>
                     </div>
                 </div>
-
-                <div className='mt-10'>
-                    <h2 className='text-xl my-3 font-medium'>Who do you plan to travel with ?</h2>
-                    <div className='grid grid-cols-3 gap-5 mt-3'>
-                        {
-                            SelectTravelesList.map((items, index) => (
-                                <div 
-                                onClick={()=>{
-                                    handleOnChange('people',items.peple)
-                                }}
-                                key={index} 
-                                className={`p-4 border rounded-lg hover:shadow-lg cursor-pointer
-                                ${formdata?.people==items.peple && 'shadow-lg border-black'}`}>
-                                    <h1>{items.icon}</h1>
-                                    <h1 className='text-2xl font-bold'>{items.title}</h1>
-                                    <h4 className='text-l text-gray-500'>{items.desc}</h4>
-                                </div>
-                            ))
-                        }
-                    </div>
-                </div>
-
-            </div>
-
-            {/* <div className='my-10 justify-end flex'>
-                <Button className>Generate Trip</Button>
-            </div> */}
-
-            <div className='my-10 justify-end flex'>
-                <button style={{
-                    width:"8rem",
-                    height:"2.5rem",
-                    backgroundColor:"Black",
-                    color:"white",
-                    borderRadius:"5px",
-                    marginBottom:'2rem',
-                    cursor:'pointer'
-                }} onClick={()=>{generateTrip()}}>Gererate Trip</button>
-            </div>
-
         </div>
-
     )
 }
 
